628113526a7c83a5cdbceb9f969912980ef31c8a
Ben Eich <jgradio64@yahoo.com>
2021-03-23 20:25:18 -0400

    Remove relocated methods

--- src/main.py
+++ src/main.py
@@ -8,17 +8,7 @@ def print_menu():
     return text
 
 
-# Extract information from the user
-def extract_info(text):
-    order = text.split(" ")
-    meal_info = order[0].lower()
-    food_info = order[1].split(",")
-    return meal_info, food_info
-
-
-order_input = "Lunch 1,2,2,2,2,2"
-# meal, foods = extract_info(order_input)
+order_input = "Lunch"
 new_order = Order()
 
 print(new_order.build_order(order_input))
-
