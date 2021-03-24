74d7bdfa93c9e4434583ad160edd12b711ef3e41
Ben Eich <jgradio64@yahoo.com>
2021-03-23 21:08:40 -0400

    Putting in a look til user decides to quit.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # order_text = order_input("Please enter an order: \t")
 from src.order import Order
 
+done = False
+
 
 # Get the user order_input
 def print_menu():
@@ -8,7 +10,8 @@ def print_menu():
     return text
 
 
-order_input = "Lunch"
-new_order = Order()
+while not done:
+    order_input = print_menu()
+    new_order = Order()
 
-print(new_order.build_order(order_input))
+    print(new_order.build_order(order_input))
