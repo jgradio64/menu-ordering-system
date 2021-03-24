07aaf49efd85b45d316c014e031eed802920b538
Ben Eich <jgradio64@yahoo.com>
2021-03-23 20:24:57 -0400

    Rename variable for clarity

--- src/meal_class.py
+++ src/meal_class.py
@@ -1,13 +1,13 @@
 class Meal:
     def __init__(self):
         # Create a dictionary to hold meal items
-        self.meals = {}
+        self.dishes = {}
         self.water_required = False
 
     # Adds a dish to the menu
     def add_dish(self, dish_name, dish_type, repeatable, needed):
         new_dish = self.Dish(dish_name, dish_type, repeatable, needed)
-        self.meals[dish_type] = new_dish
+        self.dishes[dish_type] = new_dish
 
     # Inner class dish holds
     class Dish:
